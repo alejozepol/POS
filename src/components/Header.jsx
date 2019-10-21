17ca9ab50d80c6928419943b20aec4c0e7d871a5
Alejandro Lopez <alejozepol@gmail.com>
2019-10-20 21:13:58 -0500

    creacion header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import LogoCliente from '../assets/static/LogoCliente.png';
+import IcoCaja from '../assets/static/IcoCaja.png';
+import IcoInventario from '../assets/static/IcoInventario.png';
+import '../assets/styles/components/Header.scss';
 
 const Header = () => {
   return (
-    <section>
-      <img src={LogoCliente} alt='Logo Cliente' />
+    <section className='Header'>
+      <img className='Header-img' src={LogoCliente} alt='Logo Cliente' />
+      <ul className='Header__Menu'>
+        <li className='Header__Menu__item'>
+          <img className='Header-img' src={IcoCaja} alt='Logo Cliente' />
+        </li>
+        <li className='Header__Menu__item'>
+          <img className='Header-img' src={IcoInventario} alt='Logo Cliente' />
+        </li>
+      </ul>
     </section>
   );
 };
