e82d1ececd1c75d214eb54ae360e6645e7b66de3
Alejandro Lopez <alejozepol@gmail.com>
2019-10-22 17:32:36 -0500

    creacion de reducer y actions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,66 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import reducer from './reducers';
 import App from './routes/app';
 
-ReactDom.render(<App />, document.getElementById("app"))
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialState = {
+  cart: [],
+  products: [
+    {
+      'id': '1',
+      'image': 'https://arepa.s3.amazonaws.com/camiseta.png',
+      'title': 'Camiseta',
+      'price': 25,
+      'description': 'bla bla bla bla bla',
+    },
+    {
+      'id': '3',
+      'image': 'https://arepa.s3.amazonaws.com/mug.png',
+      'title': 'Mug',
+      'price': 10,
+      'description': 'bla bla bla bla bla',
+    },
+    {
+      'id': '4',
+      'image': 'https://arepa.s3.amazonaws.com/pin.png',
+      'title': 'Pin',
+      'price': 4,
+      'description': 'bla bla bla bla bla',
+    },
+    {
+      'id': '5',
+      'image': 'https://arepa.s3.amazonaws.com/stickers1.png',
+      'title': 'Stickers',
+      'price': 2,
+      'description': 'bla bla bla bla bla',
+    },
+    {
+      'id': '6',
+      'image': 'https://arepa.s3.amazonaws.com/stickers2.png',
+      'title': 'Stickers',
+      'price': 2,
+      'description': 'bla bla bla bla bla',
+    },
+    {
+      'id': '7',
+      'image': 'https://arepa.s3.amazonaws.com/hoodie.png',
+      'title': 'Hoodie',
+      'price': 35,
+      'description': 'bla bla bla bla bla',
+    },
+
+  ],
+};
+
+const store = createStore(reducer, initialState, composeEnhacers());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
