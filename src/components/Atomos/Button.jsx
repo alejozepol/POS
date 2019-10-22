c2d03176b5aefbe5875aed19dfd99b3c17541c77
Alejandro Lopez <alejozepol@gmail.com>
2019-10-22 01:31:20 -0500

    creacion botones

--- src/components/Atomos/Button.jsx
+++ src/components/Atomos/Button.jsx
@@ -5,6 +5,10 @@ const Button = ({ type, children }) => {
   switch (type) {
     case 'redondo-aceptacion':
       return (<button type='button' className='Button Button--redondo--aceptacion'>{ children }</button>);
+    case 'lg':
+      return (<button type='button' className='Button Button--lg'>{ children }</button>);
+    case 'lg-img':
+      return (<button type='button' className='Button Button--lg--img'>{ children }</button>);
     default:
       return (<button type='button' className='Button Button--redondo'>{ children }</button>);
 
