71e2711e7a1815a756c3f1cb713b9466c569afe5
Alejandro Lopez <alejozepol@gmail.com>
2019-10-21 23:43:09 -0500

    posicion btn

--- src/container/Login.jsx
+++ src/container/Login.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ImgPrimary from '../components/Atomos/ImgPrimary';
 import PromoLogin from '../assets/static/PromoLogin.png';
 import ContentForm from '../components/Atomos/ContentForm';
-import '../assets/styles/components/Login.scss';
 import Input from '../components/Atomos/Input';
+import Button from '../components/Atomos/Button';
+import '../assets/styles/components/Login.scss';
 
 const Login = () => {
   return (
     <section className='Login'>
       <ImgPrimary src={PromoLogin} alt='PromoLogin' />
       <ContentForm title='Ingreso'>
-        <from>
+        <from className='Login__Form'>
           <Input type='top' name='Correo Electronico' />
           <Input type='bottom' name='Contraseña' />
+          <div className='btn'>
+            <Button type='redondo-aceptacion'>Conte</Button>
+          </div>
         </from>
       </ContentForm>
     </section>
