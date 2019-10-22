5de9050ce8dce39a1b2e148cc24ea2e6ef10b2fd
Alejandro Lopez <alejozepol@gmail.com>
2019-10-22 01:30:52 -0500

    crear icons

---
+++ src/components/Atomos/Icons.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import IcoGoogle from '../../assets/static/LogoGoogle.png';
+import IcoFacebook from '../../assets/static/LogoFacebook.png';
+import IcoCaja from '../../assets/static/IcoCaja.png';
+import IcoInventario from '../../assets/static/IcoInventario.png';
+import '../../assets/styles/components/Atomos/Icons.scss';
+
+const Icons = ({ type }) => {
+  switch (type) {
+    case 'check':
+      return (<i className='material-icons'> check </i>);
+    case 'google':
+      return (<img className='Icons--img' src={IcoGoogle} alt='Logo Caja' />);
+    case 'facebook':
+      return (<img className='Icons--img' src={IcoFacebook} alt='Logo Caja' />);
+    case 'caja':
+      return (<img className='Icons--img' src={IcoCaja} alt='Logo Caja' />);
+    case 'inventario':
+      return (<img className='Icons--img' src={IcoInventario} alt='Logo Inventario' />);
+    default:
+      return (<button type='button' className='Button Button--redondo'>{children}</button>);
+
+  }
+};
+
+export default Icons;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import LogoCliente from '../assets/static/LogoCliente.png';
-import IcoCaja from '../assets/static/IcoCaja.png';
-import IcoInventario from '../assets/static/IcoInventario.png';
+
 import '../assets/styles/components/Header.scss';
+import Icons from './Atomos/Icons';
 
 const Header = () => {
   return (
@@ -10,10 +10,10 @@ const Header = () => {
       <img className='Header-img' src={LogoCliente} alt='Logo Cliente' />
       <ul className='Header__Menu'>
         <li className='Header__Menu__item'>
-          <img className='Header-img' src={IcoCaja} alt='Logo Cliente' />
+          <Icons type='caja' />
         </li>
         <li className='Header__Menu__item'>
-          <img className='Header-img' src={IcoInventario} alt='Logo Cliente' />
+          <Icons type='inventario' />
         </li>
       </ul>
     </section>
